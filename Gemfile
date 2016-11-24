a8ac416b2e0868cf319a16cfe9fc2e57c4a09775
Vinchenso <chensok@gmail.com>
2016-11-24 23:10:20 +0200

    Added secure password, but without null:false

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'sqlite3'
 gem 'puma', '~> 3.0'
 gem 'jsonapi-resources'
+gem 'bcrypt'
 # gem 'rack-cors'
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124161625) do
+ActiveRecord::Schema.define(version: 20161124205539) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20161124161625) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
