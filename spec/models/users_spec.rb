ec7bb713c39419eb2ecf120a84a851c4670b6e9f
Vinchenso <chensok@gmail.com>
2016-11-25 02:44:05 +0200

    setup and tested has_secure_password

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,3 +1,9 @@
 RSpec.describe User do
   it {is_expected.to have_many :posts}
+
+  it "persists a password digest based on the password that can be used for authentication" do
+    password = "password"
+    subject = FactoryGirl.create(:user, password: password)
+    expect(subject.authenticate(password)).to eq subject
+  end
 end
